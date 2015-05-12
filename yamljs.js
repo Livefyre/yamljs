998a9cf76b09e7e2e71de20881b01b9d85d85631
Ross Pfahler <ross.pfahler@gmail.com>
2015-05-11 17:43:43 -0700

    Refactor yamljs so it can be used not just from commandline

--- yamljs.js
+++ yamljs.js
@@ -1,61 +1,11 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
-    yaml = require('js-yaml'),
-    extend = require('./util').extend,
-    docs = [];
-
-var uri = new yaml.Type('!uri', {
-    loader: {
-        kind: 'string',
-        resolver: function(object) {
-            return object;
-        }
-    }
-});
-
-var required = new yaml.Type('!required', {
-    loader: {
-        kind: 'string',
-        resolver: function(object) {
-            if (object == yaml.NIL) {
-                throw 'field is required';
-                return yaml.NIL;
-            }
-            return object;
-        }
-    }
-});
-
-var spec = new yaml.Type('!spec', {
-    loader: {
-        kind: 'object',
-        resolver: function(object) {
-            if ('value' in object) {
-                return object.value;
-            }
-            throw 'value is required';
-        }
-    }
-});
-
-var EXTENDED_LF_SCHEMA = yaml.Schema.create([uri, required, spec]);
+var fs = require('fs');
+var yaml = require('js-yaml');
+var extend = require('./util').extend;
+var mergeYaml = require('./index');
 
 var args = process.argv.slice(2);
-args.forEach(function(filename) {
-    var contents;
-    try {
-        contents = fs.readFileSync(filename, 'utf8'),
-        docs.push(yaml.safeLoad(contents, {
-            schema: EXTENDED_LF_SCHEMA
-        }));
-    } catch (e) {
-        console.error('Error reading yaml file', e);
-        process.exit(1);
-    }
-});
 
-var obj = {};
-docs.unshift(obj);
-extend.apply(this, docs)
+var obj = mergeYaml(args);
 console.log(JSON.stringify(obj));
