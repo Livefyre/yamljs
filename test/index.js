8b856888b785a2ea59466a9afd075701599968cc
klaassen <klaassen@adobe.com>
2018-07-23 14:26:42 -0700

    Add custom type for pulling values from the environment.

--- test/index.js
+++ test/index.js
@@ -6,12 +6,14 @@ var appYamlStr = fs.readFileSync(__dirname+'/fixtures/app.yaml', 'utf8');
 
 describe('.parse', function () {
   it('parses an app.yaml example', function () {
+    process.env.TEST_ENV_VAR = 'test_env_var';
     var parsed = livefyreYaml.parse(appYamlStr);
     assert.deepEqual(parsed, {
       app: {
         ga_tracking_id: 'SUCH GOOGLE',
         apis: '',
-        assets_dir: ''
+        assets_dir: '',
+        env_var: 'test_env_var'
       }
     });
   });
